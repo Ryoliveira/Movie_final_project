97fcc098225aeeabbeff4c9272e6be61c7d6de04
Ryan O <windsofryannn@yahoo.com>
2018-03-19 19:14:52 -0700

    Updated movieData.py ADDED: Try and Except for connection issues. ADDED: Documention for class
    CHANGED: Variable "self.crazy_credits" changed to "self.crazycredits"
    
    Updated README.md
    ADDED: omdb Version
    ADDED: omdb Documention link

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 "# Movie_program_final" 
 # Movie_program_final
 "# Movie_program_final" 
+
+# omdb Version == 0.8.1
+"# omdb Documentation" 
+#https://media.readthedocs.org/pdf/omdbpy/latest/omdbpy.pdf
--- movieData.py
+++ movieData.py
@@ -1,31 +1,57 @@
+"""
+Imported libraries
+"""
+import sys
 import omdb
 import requests
 from bs4 import BeautifulSoup as BS
-
-'''
-Technical Movie Options below:
-actors, awards, box_office, country, dvd,
-director, genre, language, metascore, plot,
-poster, production, rated, released, response,
-runtime, title, type, website, writer, year,
-imdb_id, imdb_rating, imdb_votes
-
-example: movie.movie_technical_info['actors']
-'''
 
 API_KEY = 'fc03d7c8'  # API Access Key
 omdb.set_default('apikey', API_KEY)
 
 
 class MovieData:
+    """Grab a Movies technical info and trivial data.
+
+    Parameters
+    ----------
+    movie_title : `str`
+            title of movie that will have its data retrieved
+    fullplot : `bool`
+            True will retrieve full plot of movie, False will retrieve summary
+    load_all_data : `bool`
+            True will run all methods to grab data, False will wait for method activation
+
+    Examples
+    --------
+    To create an object:
+    >> movie = MovieData('The Matrix', fullplot=True, load_all_data=False)
+
+    To retrieve movie specific movie info:
+    >> object.movie_technical_info['actors']
+
+    Notes
+    -----
+    movie_technical_info Options below:
+    actors, awards, box_office, country, dvd,
+    director, genre, language, metascore, plot,
+    poster, production, rated, released, response,
+    runtime, title, type, website, writer, year,
+    imdb_id, imdb_rating, imdb_votes
+    """
     def __init__(self, movie_title, fullplot=False,
                  load_all_data=False):  # full plot and load_all_data False by default
-        self.movie_technical_info = omdb.get(title=movie_title, fullplot=fullplot, tomatoes=False)
+        try:
+            self.movie_technical_info = omdb.get(title=movie_title, fullplot=fullplot, tomatoes=False)
+        except requests.exceptions.RequestException:  # This is the correct syntax
+            print("ERROR: Could Not connect to url, please check connection.")
+            sys.exit(1)
+
         self.imdb_id = self.movie_technical_info['imdb_id']
         self.trivia = []
         self.goofs = []
         self.quotes = []
-        self.crazy_credits = []
+        self.crazycredits = []
         if load_all_data:  # if true, loads all data for current movie
             self.get_trivia()
             self.get_goofs()
@@ -33,31 +59,44 @@ class MovieData:
             self.get_crazy_credits()
 
     def get_trivia(self):
-        # Grabs trivia about movie, stores as list
-        trivia_page = requests.get('http://www.imdb.com/title/' + self.imdb_id + '/trivia/?ref_=tt_trv_trv')
+        """Grabs trivia about movie, stores as list"""
+        try:
+            trivia_page = requests.get('http://www.imdb.com/title/' + self.imdb_id + '/trivia/?ref_=tt_trv_trv')
+        except requests.exceptions.RequestException:
+            print("ERROR: Could Not connect to url, please check connection.")
+            sys.exit(1)
         trivia_soup = BS(trivia_page.content, 'lxml')
-        trivia_data = trivia_soup.find(id='trivia_content')
         self.trivia = [trivia_fact.get_text().lstrip() for trivia_fact in
-                       trivia_data.find_all('div', class_="sodatext")]
+                       trivia_soup.find_all('div', class_="sodatext")]
 
     def get_goofs(self):
-        # Grabs goofs about movie, stores as list
-        goof_page = requests.get('http://www.imdb.com/title/' + self.imdb_id + '/goofs/?ref_=tt_trv_trv')
+        """Grabs goofs about movie, stores as list"""
+        try:
+            goof_page = requests.get('http://www.imdb.com/title/' + self.imdb_id + '/goofs/?ref_=tt_trv_trv')
+        except requests.exceptions.RequestException:
+            print("ERROR: Could Not connect to url, please check connection.")
+            sys.exit(1)
         goof_soup = BS(goof_page.content, 'lxml')
-        goof_data = goof_soup.find(id='goofs_content')
-        self.goofs = [goof_fact.get_text().lstrip() for goof_fact in goof_data.find_all('div', class_="sodatext")]
+        self.goofs = [goof_fact.get_text().lstrip() for goof_fact in goof_soup.find_all('div', class_="sodatext")]
 
     def get_quotes(self):
-        # # Grabs quotes from movie, stores as list
-        quotes_page = requests.get('http://www.imdb.com/title/' + self.imdb_id + '/quotes/?ref_=tt_trv_trv')
+        """Grabs quotes from movie, stores as list"""
+        try:
+            quotes_page = requests.get('http://www.imdb.com/title/' + self.imdb_id + '/quotes/?ref_=tt_trv_trv')
+        except requests.exceptions.RequestException:
+            print("ERROR: Could Not connect to url, please check connection.")
+            sys.exit(1)
         quotes_soup = BS(quotes_page.content, 'lxml')
-        quotes_data = quotes_soup.find(id='quotes_content')
-        self.quotes = [quote.get_text().lstrip() for quote in quotes_data.find_all('div', class_="sodatext")]
+        self.quotes = [quote.get_text().lstrip() for quote in quotes_soup.find_all('div', class_="sodatext")]
 
     def get_crazy_credits(self):
-        # Grabs crazy credits about movie, stores as list
-        crazycredits_page = requests.get('http://www.imdb.com/title/' + self.imdb_id + '/crazycredits/?ref_=tt_trv_trv')
+        """Grabs crazy credits about movie, stores as list"""
+        try:
+            crazycredits_page = requests.get(
+                'http://www.imdb.com/title/' + self.imdb_id + '/crazycredits/?ref_=tt_trv_trv')
+        except requests.exceptions.RequestException:
+            print("ERROR: Could Not connect to url, please check connection.")
+            sys.exit(1)
         crazycredits_page_soup = BS(crazycredits_page.content, 'lxml')
-        crazycredits_page_data = crazycredits_page_soup.find(id='crazycredits_content')
-        self.crazy_credits = [crazycredits.get_text().lstrip() for crazycredits in
-                              crazycredits_page_data.find_all('div', class_="sodatext")]
+        self.crazycredits = [crazycredits.get_text().lstrip() for crazycredits in
+                             crazycredits_page_soup.find_all('div', class_="sodatext")]
